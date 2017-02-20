006967c6707b00b95233ddf6b777547731832241
[X-Cessive] Exile Tanoa <mrsage@x-cessive.us>
2017-02-19 19:59:14 -0500

    Deleted

--- MarXet-master/MarXet-master/MarXet-SQL.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `marxet` (
-  `listingID` varchar(8) NOT NULL,
-  `itemAvailable` tinyint(1) NOT NULL DEFAULT '1',
-  `itemArray` text NOT NULL,
-  `price` double NOT NULL,
-  `sellerUID` varchar(64) NOT NULL,
-  PRIMARY KEY (`listingID`),
-  KEY `listingID` (`listingID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
