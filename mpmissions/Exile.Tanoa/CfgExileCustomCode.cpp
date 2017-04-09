7e3d2812ca97748e9bebeee18230628bc855be4f
Justin T <tinboye22@gmail.com>
2017-04-09 11:55:46 -0600

    Update CfgExileCustomCode.cpp

--- mpmissions/Exile.Tanoa/CfgExileCustomCode.cpp
+++ mpmissions/Exile.Tanoa/CfgExileCustomCode.cpp
@@ -1,58 +1,58 @@
 //SERVER
-ExileServer_system_database_connect                                     = "ExtDB3\Exile_Server_Overrides\ExileServer_system_database_connect.sqf";
-ExileServer_system_process_noobFilter                                   = "ExtDB3\Exile_Server_Overrides\ExileServer_system_process_noobFilter.sqf";
-ExileServer_world_initialize                                            = "ExtDB3\Exile_Server_Overrides\ExileServer_world_initialize.sqf";
-ExileServer_util_time_uptime                                            = "ExtDB3\Exile_Server_Overrides\ExileServer_util_time_uptime.sqf";
-ExileServer_util_time_currentTime                                       = "ExtDB3\Exile_Server_Overrides\ExileServer_util_time_currentTime.sqf";
-ExileServer_util_time_addTime                                           = "ExtDB3\Exile_Server_Overrides\ExileServer_util_time_addTime.sqf";
+ExileServer_system_database_connect                                     = "Exile_Server_Overrides\ExileServer_system_database_connect.sqf";
+ExileServer_system_process_noobFilter                                   = "Exile_Server_Overrides\ExileServer_system_process_noobFilter.sqf";
+ExileServer_world_initialize                                            = "Exile_Server_Overrides\ExileServer_world_initialize.sqf";
+ExileServer_util_time_uptime                                            = "Exile_Server_Overrides\ExileServer_util_time_uptime.sqf";
+ExileServer_util_time_currentTime                                       = "Exile_Server_Overrides\ExileServer_util_time_currentTime.sqf";
+ExileServer_util_time_addTime                                           = "Exile_Server_Overrides\ExileServer_util_time_addTime.sqf";
 
 //TRADING
-ExileServer_system_trading_network_wasteDumpRequest                     = "ExtDB3\Exile_Server_Overrides\ExileServer_system_trading_network_wasteDumpRequest.sqf";
-ExileServer_system_trading_network_sellItemRequest                      = "ExtDB3\Exile_Server_Overrides\ExileServer_system_trading_network_sellItemRequest.sqf";
-ExileServer_system_trading_network_purchaseVehicleSkinRequest           = "ExtDB3\Exile_Server_Overrides\ExileServer_system_trading_network_purchaseVehicleSkinRequest.sqf";
-ExileServer_system_trading_network_purchaseVehicleRequest               = "ExtDB3\Exile_Server_Overrides\ExileServer_system_trading_network_purchaseVehicleRequest.sqf";
-ExileServer_system_trading_network_purchaseItemRequest                  = "ExtDB3\Exile_Server_Overrides\ExileServer_system_trading_network_purchaseItemRequest.sqf";
+ExileServer_system_trading_network_wasteDumpRequest                     = "Exile_Server_Overrides\ExileServer_system_trading_network_wasteDumpRequest.sqf";
+ExileServer_system_trading_network_sellItemRequest                      = "Exile_Server_Overrides\ExileServer_system_trading_network_sellItemRequest.sqf";
+ExileServer_system_trading_network_purchaseVehicleSkinRequest           = "Exile_Server_Overrides\ExileServer_system_trading_network_purchaseVehicleSkinRequest.sqf";
+ExileServer_system_trading_network_purchaseVehicleRequest               = "Exile_Server_Overrides\ExileServer_system_trading_network_purchaseVehicleRequest.sqf";
+ExileServer_system_trading_network_purchaseItemRequest                  = "Exile_Server_Overrides\ExileServer_system_trading_network_purchaseItemRequest.sqf";
 
 //TERRITORY
-ExileServer_system_territory_network_territoryUpgradeRequest            = "ExtDB3\Exile_Server_Overrides\ExileServer_system_territory_network_territoryUpgradeRequest.sqf";
-ExileServer_system_territory_network_restoreFlagRequest                 = "ExtDB3\Exile_Server_Overrides\ExileServer_system_territory_network_restoreFlagRequest.sqf";
-ExileServer_system_territory_network_purchaseTerritory                  = "ExtDB3\Exile_Server_Overrides\ExileServer_system_territory_network_purchaseTerritory.sqf";
-ExileServer_system_territory_network_payTerritoryProtectionMoneyRequest = "ExtDB3\Exile_Server_Overrides\ExileServer_system_territory_network_payTerritoryProtectionMoneyRequest.sqf";
-ExileServer_system_territory_network_payFlagRansomRequest               = "ExtDB3\Exile_Server_Overrides\ExileServer_system_territory_network_payFlagRansomRequest.sqf";
-ExileServer_system_territory_network_flagStolenRequest                  = "ExtDB3\Exile_Server_Overrides\ExileServer_system_territory_network_flagStolenRequest.sqf";
-ExileServer_system_territory_maintenance_recalculateDueDate             = "ExtDB3\Exile_Server_Overrides\ExileServer_system_territory_maintenance_recalculateDueDate.sqf";
-ExileServer_system_territory_database_insert                            = "ExtDB3\Exile_Server_Overrides\ExileServer_system_territory_database_insert.sqf";
+ExileServer_system_territory_network_territoryUpgradeRequest            = "Exile_Server_Overrides\ExileServer_system_territory_network_territoryUpgradeRequest.sqf";
+ExileServer_system_territory_network_restoreFlagRequest                 = "Exile_Server_Overrides\ExileServer_system_territory_network_restoreFlagRequest.sqf";
+ExileServer_system_territory_network_purchaseTerritory                  = "Exile_Server_Overrides\ExileServer_system_territory_network_purchaseTerritory.sqf";
+ExileServer_system_territory_network_payTerritoryProtectionMoneyRequest = "Exile_Server_Overrides\ExileServer_system_territory_network_payTerritoryProtectionMoneyRequest.sqf";
+ExileServer_system_territory_network_payFlagRansomRequest               = "Exile_Server_Overrides\ExileServer_system_territory_network_payFlagRansomRequest.sqf";
+ExileServer_system_territory_network_flagStolenRequest                  = "Exile_Server_Overrides\ExileServer_system_territory_network_flagStolenRequest.sqf";
+ExileServer_system_territory_maintenance_recalculateDueDate             = "Exile_Server_Overrides\ExileServer_system_territory_maintenance_recalculateDueDate.sqf";
+ExileServer_system_territory_database_insert                            = "Exile_Server_Overrides\ExileServer_system_territory_database_insert.sqf";
 
 //DB QUERIES
-ExileServer_system_database_query_selectSingleField                     = "ExtDB3\Exile_Server_Overrides\ExileServer_system_database_query_selectSingleField.sqf";
-ExileServer_system_database_query_selectSingle                          = "ExtDB3\Exile_Server_Overrides\ExileServer_system_database_query_selectSingle.sqf";
-ExileServer_system_database_query_selectFull                            = "ExtDB3\Exile_Server_Overrides\ExileServer_system_database_query_selectFull.sqf";
-ExileServer_system_database_query_insertSingle                          = "ExtDB3\Exile_Server_Overrides\ExileServer_system_database_query_insertSingle.sqf";
-ExileServer_system_database_query_fireAndForget                         = "ExtDB3\Exile_Server_Overrides\ExileServer_system_database_query_fireAndForget.sqf";
-ExileServer_system_database_handleBig                                   = "ExtDB3\Exile_Server_Overrides\ExileServer_system_database_handleBig.sqf";
+ExileServer_system_database_query_selectSingleField                     = "Exile_Server_Overrides\ExileServer_system_database_query_selectSingleField.sqf";
+ExileServer_system_database_query_selectSingle                          = "Exile_Server_Overrides\ExileServer_system_database_query_selectSingle.sqf";
+ExileServer_system_database_query_selectFull                            = "Exile_Server_Overrides\ExileServer_system_database_query_selectFull.sqf";
+ExileServer_system_database_query_insertSingle                          = "Exile_Server_Overrides\ExileServer_system_database_query_insertSingle.sqf";
+ExileServer_system_database_query_fireAndForget                         = "Exile_Server_Overrides\ExileServer_system_database_query_fireAndForget.sqf";
+ExileServer_system_database_handleBig                                   = "Exile_Server_Overrides\ExileServer_system_database_handleBig.sqf";
 
 //VEHICLES
-ExileServer_object_vehicle_database_load                                = "ExtDB3\Exile_Server_Overrides\ExileServer_object_vehicle_database_load.sqf";
-ExileServer_object_vehicle_createPersistentVehicle                      = "ExtDB3\Exile_Server_Overrides\ExileServer_object_vehicle_createPersistentVehicle.sqf";
-ExileServer_object_vehicle_createNonPersistentVehicle                   = "ExtDB3\Exile_Server_Overrides\ExileServer_object_vehicle_createNonPersistentVehicle.sqf";
+ExileServer_object_vehicle_database_load                                = "Exile_Server_Overrides\ExileServer_object_vehicle_database_load.sqf";
+ExileServer_object_vehicle_createPersistentVehicle                      = "Exile_Server_Overrides\ExileServer_object_vehicle_createPersistentVehicle.sqf";
+ExileServer_object_vehicle_createNonPersistentVehicle                   = "Exile_Server_Overrides\ExileServer_object_vehicle_createNonPersistentVehicle.sqf";
 
 //PLAYER
-ExileServer_object_player_event_onMpKilled                              = "ExtDB3\Exile_Server_Overrides\ExileServer_object_player_event_onMpKilled.sqf";
-ExileServer_object_player_database_load                                 = "ExtDB3\Exile_Server_Overrides\ExileServer_object_player_database_load.sqf";
-ExileServer_object_player_createBambi                                   = "ExtDB3\Exile_Server_Overrides\ExileServer_object_player_createBambi.sqf";
+ExileServer_object_player_event_onMpKilled                              = "Exile_Server_Overrides\ExileServer_object_player_event_onMpKilled.sqf";
+ExileServer_object_player_database_load                                 = "Exile_Server_Overrides\ExileServer_object_player_database_load.sqf";
+ExileServer_object_player_createBambi                                   = "Exile_Server_Overrides\ExileServer_object_player_createBambi.sqf";
 
 //CONTAINERS
-ExileServer_object_container_database_update                            = "ExtDB3\Exile_Server_Overrides\ExileServer_object_container_database_update.sqf";
-ExileServer_object_container_database_load                              = "ExtDB3\Exile_Server_Overrides\ExileServer_object_container_database_load.sqf";
-ExileServer_object_container_database_insert                            = "ExtDB3\Exile_Server_Overrides\ExileServer_object_container_database_insert.sqf";
-ExileServer_object_container_createContainer                            = "ExtDB3\Exile_Server_Overrides\ExileServer_object_container_createContainer.sqf";
+ExileServer_object_container_database_update                            = "Exile_Server_Overrides\ExileServer_object_container_database_update.sqf";
+ExileServer_object_container_database_load                              = "Exile_Server_Overrides\ExileServer_object_container_database_load.sqf";
+ExileServer_object_container_database_insert                            = "Exile_Server_Overrides\ExileServer_object_container_database_insert.sqf";
+ExileServer_object_container_createContainer                            = "Exile_Server_Overrides\ExileServer_object_container_createContainer.sqf";
 
 //CONSTRUCTION
-ExileServer_object_construction_database_load                           = "ExtDB3\Exile_Server_Overrides\ExileServer_object_construction_database_load.sqf";
-ExileServer_object_construction_database_insert                         = "ExtDB3\Exile_Server_Overrides\ExileServer_object_construction_database_insert.sqf";
+ExileServer_object_construction_database_load                           = "Exile_Server_Overrides\ExileServer_object_construction_database_load.sqf";
+ExileServer_object_construction_database_insert                         = "Exile_Server_Overrides\ExileServer_object_construction_database_insert.sqf";
 
 //CLIENT
-ExileClient_util_string_scalarToString                                  = "ExtDB3\Exile_Client_Overrides\ExileClient_util_string_scalarToString.sqf";
+ExileClient_util_string_scalarToString                                  = "Exile_Client_Overrides\ExileClient_util_string_scalarToString.sqf";
 
 //THESE FILES ARE COMBINED WITHIN THE EXTDB3 FILES ABOVE.
 //ExileServer_object_player_createBambi                                   = "custom\loadout\ExileServer_object_player_createBambi.sqf";
